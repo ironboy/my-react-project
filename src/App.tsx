28133fa103a0601bfaac9ee209c56cff97707bcd
Ironboy <ironboy@users.noreply.github.com>
2025-09-09 13:58:22 +0200

    search added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import useFetchJson from "./utils/useFetchJson";
 
 interface User {
@@ -10,12 +11,35 @@ interface User {
 
 export default function App() {
 
+  const [search, setSearch] = useState('');
   const users = useFetchJson<User[]>('/json/users.json');
 
-  return <>
-    {users?.map(({ firstName }, i) => <h3 key={i}>
-      {firstName}
-    </h3>)}
-  </>;
+  // Search/filters the users array
+  // by the search phrase (search)
+  const found = users?.filter(({ firstName, lastName }) => {
+    const wholeName = firstName + ' ' + lastName;
+    return wholeName.toLowerCase().includes(search.toLowerCase());
+  });
+
+  return <main>
+    <label>
+      Search:&nbsp;
+      <input
+        type="text"
+        value={search}
+        onChange={event => setSearch(event.target.value)}
+        placeholder="Search by name"
+      />
+    </label>
+
+    {found?.map(({
+      id, firstName, lastName, email, phone
+    }) => <article key={id}>
+        <h3>{firstName} {lastName}</h3>
+        <p><b>Email: </b>{email}</p>
+        <p><b>Phone: </b>{phone}</p>
+      </article>
+    )}
+  </main>;
 
 }
