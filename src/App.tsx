2566434858a09f3ada6f7aa2eccafb32e4fca677
Ironboy <ironboy@users.noreply.github.com>
2025-09-09 11:44:43 +0200

    cleaned old example from this branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { useState } from 'react';
-import Greeter from "./Greeter";
-import NameInput from './NameInput';
-
 export default function App() {
-
-  const [greeting, setGreeting] = useState('Hi');
-  const [names, setNames] = useState('Jane Doe');
-
   return <>
-    <NameInput {...{ names, setNames }} />
-    {/* Mounting the Greeter component */}
-    <Greeter  {...{ greeting, setGreeting, names }} />
+    <h1>Hello World!</h1>
   </>;
 }
