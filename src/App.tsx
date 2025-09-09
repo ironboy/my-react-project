75ff4ee3da99489ba1300a8495c00f09cdcdea25
Ironboy <ironboy@users.noreply.github.com>
2025-09-09 11:09:43 +0200

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import Greeter from "./Greeter";
 
 export default function App() {
+
+  const [greeting, setGreeting] = useState('Hi');
+  const [name, setName] = useState('Jane Doe');
+
   return <>
-    <Greeter />
+    {/* A state controlled input */}
+    <input
+      value={name}
+      onChange={(event) => setName(event.target.value)}
+    />
+    {/* Mounting the Greeter component */}
+    <Greeter  {...{ greeting, setGreeting, name }} />
   </>;
 }
--- src/Greeter.tsx
+++ src/Greeter.tsx
@@ -1,3 +1,14 @@
-export default function Greeter() {
-  return <h1>Good morning Linköping!</h1>;
+export default function Greeter(
+  { greeting, setGreeting, name }:
+    { greeting: string; setGreeting: Function, name: string; }
+) {
+  return <>
+    <p>Choose a greeting:</p>
+    <p>
+      <button onClick={() => setGreeting('Hi')}>Hi</button>
+      <button onClick={() => setGreeting('Hello')}>Hello</button>
+      <button onClick={() => setGreeting('Yo')}>Yo</button>
+    </p>
+    <h1>{greeting} {name}!</h1>
+  </>;
 }
