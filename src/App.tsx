5b87131ec372ef2d8bed5848223790c8bab8a77d
Ironboy <ironboy@users.noreply.github.com>
2025-09-09 11:37:13 +0200

    exercise 3 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Greeter from "./Greeter";
+import NameInput from './NameInput';
 
 export default function App() {
 
@@ -7,13 +8,7 @@ export default function App() {
   const [names, setNames] = useState('Jane Doe');
 
   return <>
-    {/* A state controlled input */}
-    <label>Comma-separated list of names:&nbsp;
-      <input type="text" id="nameInput"
-        value={names}
-        onChange={(event) => setNames(event.target.value)}
-      />
-    </label>
+    <NameInput {...{ names, setNames }} />
     {/* Mounting the Greeter component */}
     <Greeter  {...{ greeting, setGreeting, names }} />
   </>;
